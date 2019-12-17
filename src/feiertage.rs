123c5a0fdccbae23d06560c3b97e1e9b2a15f7ac
Olaf Leidinger <oleid@mescharet.de>
2019-12-17 23:52:56 +0100

    Cache feiertage api

--- src/feiertage.rs
+++ src/feiertage.rs
@@ -3,21 +3,23 @@ extern crate failure;
 extern crate reqwest;
 extern crate serde_json;
 
+use directories::ProjectDirs;
 use std::collections::btree_map::BTreeMap;
+use std::fs::File;
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Serialize)]
 struct ApiResponse {
     #[serde(flatten)]
     inner: BTreeMap<String, HolidayInfo>,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Serialize)]
 struct HolidayInfo {
     datum: chrono::NaiveDate,
     hinweis: String,
 }
 
-#[derive(Debug)]
+#[derive(Copy, Clone, Debug)]
 #[allow(dead_code)]
 pub enum Bundesland {
     BE, // Berlin
@@ -38,10 +40,7 @@ pub enum Bundesland {
     TH, //	Thüringen (Erfurt)
 }
 
-pub fn get_holidays(
-    year: i32,
-    state: Bundesland,
-) -> Result<BTreeMap<chrono::NaiveDate, String>, failure::Error> {
+fn query(year: i32, state: Bundesland) -> Result<ApiResponse, failure::Error> {
     let client = reqwest::Client::new();
 
     let url = format!(
@@ -51,17 +50,70 @@ pub fn get_holidays(
 
     let mut req = client.get(&url).send()?;
 
+    debug!("Answer from {} : {}", url, req.status());
+
     if req.status() != 200 {
-        panic!("Unexpected http status : {}", req.status())
+        return Err(failure::err_msg(format!(
+            "Unexpected http status {} when reading {} ",
+            req.status(),
+            url
+        )));
     }
 
-    let response = req.json::<ApiResponse>()?;
+    req.json::<ApiResponse>()
+        .map(|response| {
+            persist(year, state, &response).unwrap_or_default();
+            response
+        })
+        .map_err(|e| e.into())
+}
 
-    Ok(response
-        .inner
-        .into_iter()
-        .map(|(k, v)| (v.datum, k.into()))
-        .collect())
+fn load_cache(year: i32, state: Bundesland) -> Result<ApiResponse, failure::Error> {
+    if let Some(proj_dirs) = ProjectDirs::from("org", "Leidingerware", "redmine-helper") {
+        let cache_file = proj_dirs
+            .cache_dir()
+            .join(format!("feiertage_{}_{:?}.json", year, state));
+
+        debug!("Trying cache file: {}", cache_file.display());
+
+        let file = File::open(&cache_file)?;
+
+        serde_json::from_reader::<File, ApiResponse>(file).map_err(|e| e.into())
+    } else {
+        Err(failure::err_msg("Could not load cache file"))
+    }
+}
+
+fn persist(year: i32, state: Bundesland, response: &ApiResponse) -> Result<(), failure::Error> {
+    if let Some(proj_dirs) = ProjectDirs::from("org", "Leidingerware", "redmine-helper") {
+        let cache_dir = proj_dirs.cache_dir();
+        std::fs::create_dir_all(cache_dir)?;
+
+        let cache_file = cache_dir.join(format!("feiertage_{}_{:?}.json", year, state));
+
+        debug!("Trying to write cache file: {}", cache_file.display());
+
+        let file = File::create(&cache_file)?;
+
+        serde_json::to_writer(file, response).map_err(|e| e.into())
+    } else {
+        Err(failure::err_msg("Could not load cache file"))
+    }
+}
+
+pub fn get_holidays(
+    year: i32,
+    state: Bundesland,
+) -> Result<BTreeMap<chrono::NaiveDate, String>, failure::Error> {
+    load_cache(year, state)
+        .or_else(|_| query(year, state))
+        .and_then(|response| {
+            Ok(response
+                .inner
+                .into_iter()
+                .map(|(k, v)| (v.datum, k.into()))
+                .collect())
+        })
 }
 
 #[test]
